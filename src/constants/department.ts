36a5456626979bfac742c2fa18af29b5ceaa9e81
sital999 <sitalnagarkoti123@gmail.com>
2023-04-23 18:12:10 +0545

    create department-section-cms

---
+++ src/constants/department.ts
@@ -0,0 +1,22 @@
+export const Departments = [
+  {
+    name: 'Computer',
+    semesters: '1st Sem',
+  },
+  {
+    name: 'Civil',
+    semesters: '2nd Sem',
+  },
+  {
+    name: 'Electronics',
+    semesters: '1st Sem',
+  },
+  {
+    name: 'Electrical',
+    semesters: '3rd Sem',
+  },
+  {
+    name: 'Mechanical',
+    semesters: '4th Sem',
+  },
+];
