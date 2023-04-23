facc3aeabd235b4125111ae0cbbf8251224171c1
sital999 <sitalnagarkoti123@gmail.com>
2023-04-23 18:35:58 +0545

    make constant for ui on subjects

---
+++ src/constants/subjects.ts
@@ -0,0 +1,32 @@
+export const Subjects = [
+  {
+    title: 'Engineering Mathematics II',
+    subject_code: 'MEII',
+    description: 'Subject comprises...',
+    chapters: 'Dimensioning',
+  },
+  {
+    title: 'Engineering Mathematics II',
+    subject_code: 'MEII',
+    description: 'Subject comprises...',
+    chapters: 'Dimensioning',
+  },
+  {
+    title: 'Engineering Mathematics II',
+    subject_code: 'MEII',
+    description: 'Subject comprises...',
+    chapters: 'Dimensioning',
+  },
+  {
+    title: 'Engineering Mathematics II',
+    subject_code: 'MEII',
+    description: 'Subject comprises...',
+    chapters: 'Dimensioning',
+  },
+  {
+    title: 'Engineering Mathematics II',
+    subject_code: 'MEII',
+    description: 'Subject comprises...',
+    chapters: 'Dimensioning',
+  },
+];
