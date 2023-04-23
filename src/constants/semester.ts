8a9dd9de9ffbbf7376a00d53fcd09d0f086bbc4d
sital999 <sitalnagarkoti123@gmail.com>
2023-04-23 18:50:29 +0545

    make semester-section-cms

---
+++ src/constants/semester.ts
@@ -0,0 +1,50 @@
+export const Semesters = [
+  {
+    value: '1',
+    start_Date: '1st Mangsir 2080',
+    end_Date: '30 Chaitra 2080',
+    subjects: 'Physics,Math...',
+  },
+  {
+    value: '2',
+    start_Date: '1st Baisakh 2081',
+    end_Date: '30 Bhadra 2081',
+    subjects: 'Digital Logic,Math...',
+  },
+  {
+    value: '3',
+    start_Date: '1st Mangsir 2081',
+    end_Date: '30 Chaitra 2081',
+    subjects: 'C++,Thermodynamics...',
+  },
+  {
+    value: '4',
+    start_Date: '1st Baisakh 2082',
+    end_Date: '30 Bhadra 2082',
+    subjects: 'Instrumentation,Math...',
+  },
+  {
+    value: '5',
+    start_Date: '1st Mangsir 2082',
+    end_Date: '30 Chaitra 2082',
+    subjects: 'COA,Instrumentation II...',
+  },
+  {
+    value: '6',
+    start_Date: '1st Baisakh 2083',
+    end_Date: '30 Bhadra 2083',
+    subjects: 'AI,Embedded System...',
+  },
+  {
+    value: '7',
+    start_Date: '1st Mangsir 2083',
+    end_Date: '30 Chaitra 2083',
+    subjects: 'Project Management...',
+  },
+  {
+    value: '8',
+    start_Date: '1st Baisakh 2083',
+    end_Date: '30 Bhadra 2083',
+    subjects: 'Information System...',
+  },
+];
